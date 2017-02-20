715e7809df3a56bc48df49941d9b1930304c7363
Jacobo Quintáns <jacoboqc@gmail.com>
2017-02-20 21:58:25 +0100

    Send POST with received MAC

--- receiver/receiver.py
+++ receiver/receiver.py
@@ -1,6 +1,7 @@
 import sys
 import pyshark
 import requests
+import json
 from mac import Mac
 
 if len(sys.argv) == 1:
@@ -15,6 +16,7 @@ capture = pyshark.LiveCapture(interface=intSniff, display_filter='wlan.fc.type_s
 try:
     for packet in capture.sniff_continuously():
         mac = Mac(packet.wlan.sa)
+        requests.post('localhost:3000/macs', data={json.dumps(mac.__dict__)})
         print (mac.mac, mac.time)
 except KeyboardInterrupt:
     print ('Shutting down...')
