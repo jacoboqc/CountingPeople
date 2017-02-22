260e170224b75ab9ed545eafaba418e13fdcf734
Jacobo Quintáns <jacoboqc@gmail.com>
2017-02-22 23:01:35 +0100

    Send MACs to server

--- receiver/mac.py
+++
@@ -1,8 +0,0 @@
-import time
-
-class Mac:
-    def __init__(self, mac):
-        self.mac = mac
-        self.id = 1
-        self.time = time.strftime("%x-%X")
-        self.device = "Android"
--- receiver/receiver.py
+++ receiver/receiver.py
@@ -1,11 +1,10 @@
 import sys
 import pyshark
 import requests
-import json
-from mac import Mac
+import time
 
 if len(sys.argv) == 1:
-    print ('Usage: python sniffer.py interface')
+    print ('Usage: python receiver.py interface')
     sys.exit()
 
 print ('Starting WiFi sniffer...')
@@ -15,9 +14,11 @@ print ('Using interface', intSniff)
 capture = pyshark.LiveCapture(interface=intSniff, display_filter='wlan.fc.type_subtype==4')
 try:
     for packet in capture.sniff_continuously():
-        mac = Mac(packet.wlan.sa)
-        requests.post('localhost:3000/macs', data={json.dumps(mac.__dict__)})
-        print (mac.mac, mac.time)
+        m = packet.wlan.sa
+        t = time.strftime("%x-%X")
+        j = {"mac":m, "origin":{"id":1, "time":t}, "device":"Android"}
+        requests.put('http://localhost:3001/macs', json=j)
+        print (m, t)
 except KeyboardInterrupt:
     print ('Shutting down...')
     sys.exit()
