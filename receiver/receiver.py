31f24d52235584c54f6f599e5d88d687caa0e2ff
Jacobo Quintáns <jacoboqc@gmail.com>
2017-02-23 13:03:33 +0100

    Read params from config file

--- receiver/receiver.py
+++ receiver/receiver.py
@@ -1,23 +1,37 @@
+#!/usr/bin/python
+
 import sys
 import pyshark
 import requests
 import time
+import configparser
 
-if len(sys.argv) == 1:
-    print ('Usage: python receiver.py interface')
+if len(sys.argv) > 1 and sys.argv[1] == 'help':
+    print ('Usage: receiver.py [help]')
+    print ('Config file must be placed in the same directory and named `default.conf`.')
     sys.exit()
 
-print ('Starting WiFi sniffer...')
-intSniff = sys.argv[1]
-print ('Using interface', intSniff)
+c = configparser.ConfigParser()
+c.read('default.conf')
+id = c.getint('general', 'Receiver ID')
+u = c.get('general', 'API URL')
+p = c.get('general', 'API Port')
 
-capture = pyshark.LiveCapture(interface=intSniff, display_filter='wlan.fc.type_subtype==4')
+if c.get('general', 'Mode') == 'live':
+    i = c.get('live', 'Interface')
+elif c.get('general', 'Mode') == 'file':
+    f = c.get('file', 'File Name')
+
+print ('Starting WiFi sniffer...')
+print ('Using interface', i)
+
+capture = pyshark.LiveCapture(interface=i, display_filter='wlan.fc.type_subtype==4')
 try:
     for packet in capture.sniff_continuously():
         m = packet.wlan.sa
         t = time.strftime("%x-%X")
-        j = {"mac":m, "origin":{"id":1, "time":t}, "device":"Android"}
-        requests.put('http://localhost:3001/macs', json=j)
+        j = {"mac":m, "origin":{"id":id, "time":t}, "device":"Android"}
+        requests.put('http://'+u+':'+p+'/macs', json=j)
         print (m, t)
 except KeyboardInterrupt:
     print ('Shutting down...')
