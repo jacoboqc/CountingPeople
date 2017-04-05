96cc21df37c2fb7adb74fa102f2e441c21a78ccc
ledermauss <mauropdr@gmail.com>
2017-04-05 23:51:33 +0200

    Minor changes in server, copied first functions from Ipython static analysis (origin_activity)

--- stats/real-time/server.R
+++ stats/real-time/server.R
@@ -14,7 +14,8 @@ library(ggplot2)
 source("./server-functions.R")
 
 refresh <- 5
-begin <- as.POSIXct("2017-03-19 02:04:42 CEST")
+# begin <- Sys.time() - 1200
+begin <- as.POSIXct("2017-03-31 12:20:40 CEST")
 end <- begin + refresh
 mac_df <- data.frame()
 mac_list <- data.frame()
@@ -24,8 +25,6 @@ shinyServer(function(input, output, session) {
 
   output$time_evol <- renderPrint({
     invalidateLater(sec2milis(refresh), session)
-    begin <<- begin + refresh
-    end <<- end + refresh
     mac_list <<- getAllMacsByTimestamp(begin=begin, end=end)
     # woul not be necessary if filtering was done at the api
     # important: interval open on one side
@@ -34,36 +33,35 @@ shinyServer(function(input, output, session) {
     cat(file=stderr(),"columns in temporal list", ncol(mac_list), "\n")
     cat(file=stderr(),"rows in permanent list", nrow(mac_df), "\n")
     cat(file=stderr(),"columns in permanent list", ncol(mac_df), "\n")
-    names(mac_list) <<- c("MAC", "device", "ID","timestamp") # important dont move
+    names(mac_list) <<- c("MAC", "device", "ID","timestamp", "type") # important dont move
     # solves match.names problems in rbind
     mac_df <<- rbind(mac_df, mac_list)
     cat(file=stderr(), "names of permanent and temporal dataset", 
         names(mac_df), names(mac_list), "\n")
     print(begin)
     print(end)
+    begin <<- begin + refresh
+    end <<- end + refresh
   })
   
   # plots amount inside the system (not fixed yet)
   output$devices_inside <- renderPlot({
     invalidateLater(sec2milis(refresh), session)
-    mac_df$entering <- addEnteringStatus(mac_df)
-    mac_df$inside <- addAmountInside(mac_df)
-    ggplot(mac_df, aes(timestamp,inside)) + geom_line()
+    mac_df$inside <- n_distinct(mac_df$MAC)
+    ggplot(mac_df, aes(timestamp,inside)) + geom_col()
   })
   
   # amount of unique macs in the interval
   output$devices_seen_interval <- renderPlot({
     invalidateLater(sec2milis(refresh), session)
-    mac_list$entering <- addEnteringStatus(mac_list)
-    mac_list$inside <- addAmountInside(mac_list)
-    ggplot(mac_list, aes(timestamp,inside)) + geom_line()
+    mac_list$inside <- n_distinct(mac_list$MAC)/2
+    ggplot(mac_list, aes(timestamp,inside)) + geom_col()
   })
   
   output$devices_seen_total <- renderPlot({
     invalidateLater(sec2milis(refresh), session)
-    mac_df$entering <- addEnteringStatus(mac_df)
-    mac_df$inside <- addAmountInside(mac_df)
-    ggplot(mac_df, aes(timestamp,inside)) + geom_line()
+    mac_df$inside <- n_distinct(mac_df$MAC)/10
+    ggplot(mac_df, aes(timestamp,inside)) + geom_col()
     
   })
   
