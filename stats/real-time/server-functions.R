396a7236d535b2190f619a7bd73a3e1b1be63d3c
ledermauss <mauropdr@gmail.com>
2017-04-06 08:13:46 +0200

    Real time. Count new macs in the system and simple count

--- stats/real-time/server-functions.R
+++ stats/real-time/server-functions.R
@@ -2,32 +2,6 @@ library(dplyr)
 library(tidyr)
 library(httr)
 
-read_capture <- function(filepath, timeformat = "%Y/%m/%d-%H:%M:%S"){
-  data <- read.csv(filepath)
-  data$time <- as.POSIXct(strptime(data$time, timeformat))
-  data
-}
-
-distinct_macs_interval <- function(data, time_col, mac_col, interval){
-  intervals <- cut(data[[time_col]], interval)
-  data[,time_col] <- intervals
-  data %>% group_by_(time_col) %>% count_(mac_col)
-  }
-
-addEnteringStatus <- function(data){
-  grouped <- data %>% group_by(MAC) %>%  mutate(
-    entering=as.logical(rank(timestamp) %% 2))
-  # Check if column "entering" changes each time a MAC appears
-  return(ungroup(grouped)$entering)
-}
-
-addAmountInside <- function(data){
-  # Amount of people in each timestamp: sum of "T" until then
-  accumulated <- data %>% mutate(
-    inside=cumsum(entering)-cumsum(!entering))
-  return(accumulated$inside)
-}
-
 getAllMacs <- function(url, timeformat = "%Y/%m/%d-%H:%M:%S"){
   r <- GET(url, accept("text/csv"))  
   data <- read.csv(text=content(r, "text"), header=T)
--- stats/real-time/server.R
+++ stats/real-time/server.R
@@ -4,67 +4,113 @@
 #
 # http://shiny.rstudio.com
 #
-
 library(shiny)
 library(dplyr)
 library(tidyr)
 library(httr)
 library(ggplot2)
+library(scales)
+library(grid)
+library(RColorBrewer)
 
 source("./server-functions.R")
+source("../static-analysis.R")
 
 refresh <- 5
 # begin <- Sys.time() - 1200
-begin <- as.POSIXct("2017-03-31 12:20:40 CEST")
+begin <- as.POSIXct("2017-03-31 12:08:40 CEST")
 end <- begin + refresh
 mac_df <- data.frame()
-mac_list <- data.frame()
+mac_temp <- data.frame()
 sec2milis <- function(x){x*1000}
+delete <- T
 
 shinyServer(function(input, output, session) {
 
   output$time_evol <- renderPrint({
+    refresh <<- input$s
+    cat(file=stderr(),refresh, "------------------- \n")
+    cat(file=stderr(),"Before the API request", "\n")
+    
     invalidateLater(sec2milis(refresh), session)
-    mac_list <<- getAllMacsByTimestamp(begin=begin, end=end)
+    mac_temp <<- getAllMacsByTimestamp(begin=begin, end=end)
     # woul not be necessary if filtering was done at the api
     # important: interval open on one side
-    mac_list <<- mac_list[mac_list$time >= begin & mac_list$time < end,]
-    cat(file=stderr(),"rows in temporal list", nrow(mac_list), "\n")
-    cat(file=stderr(),"columns in temporal list", ncol(mac_list), "\n")
+    mac_temp <<- mac_temp[mac_temp$time >= begin & mac_temp$time < end,]
+    
+    cat(file=stderr(),"rows in temporal list", nrow(mac_temp), "\n")
+    cat(file=stderr(),"columns in temporal list", ncol(mac_temp), "\n")
     cat(file=stderr(),"rows in permanent list", nrow(mac_df), "\n")
     cat(file=stderr(),"columns in permanent list", ncol(mac_df), "\n")
-    names(mac_list) <<- c("MAC", "device", "ID","timestamp", "type") # important dont move
+    names(mac_temp) <<- c("mac", "device", "ID","time", "type") # important dont move
     # solves match.names problems in rbind
-    mac_df <<- rbind(mac_df, mac_list)
+    mac_df <<- rbind(mac_df, mac_temp)
     cat(file=stderr(), "names of permanent and temporal dataset", 
-        names(mac_df), names(mac_list), "\n")
+        names(mac_df), names(mac_temp), "\n")
     print(begin)
     print(end)
-    begin <<- begin + refresh
-    end <<- end + refresh
+    begin <<- begin + input$s
+    end <<- end + input$s
   })
   
-  # plots amount inside the system (not fixed yet)
-  output$devices_inside <- renderPlot({
+  
+  output$macs_per_second <- renderPlot({
     invalidateLater(sec2milis(refresh), session)
-    mac_df$inside <- n_distinct(mac_df$MAC)
-    ggplot(mac_df, aes(timestamp,inside)) + geom_col()
+    interval_mac_count <- count_macs_interval(mac_temp, "time", "mac", "1 sec")
+    plot_date_count(interval_mac_count, "time", "mac_count", "1 sec")
   })
   
-  # amount of unique macs in the interval
-  output$devices_seen_interval <- renderPlot({
+  output$macs_per_second_total <- renderPlot({
     invalidateLater(sec2milis(refresh), session)
-    mac_list$inside <- n_distinct(mac_list$MAC)/2
-    ggplot(mac_list, aes(timestamp,inside)) + geom_col()
-  })
-  
-  output$devices_seen_total <- renderPlot({
-    invalidateLater(sec2milis(refresh), session)
-    mac_df$inside <- n_distinct(mac_df$MAC)/10
-    ggplot(mac_df, aes(timestamp,inside)) + geom_col()
+    interval_mac_count <- count_macs_interval(mac_df, "time", "mac", "1 sec")
+    plot_date_count(interval_mac_count, "time", "mac_count", "1 sec")
     
   })
   
+  # amount of unique macs in the interval
+  output$new_macs_per_second <- renderPlot({
+    invalidateLater(sec2milis(refresh), session)
+    new_macs_count <- count_new_macs_interval(mac_temp, "time", "mac", "1 sec")
+    plot_date_count(new_macs_count, "time", "mac_count", "1 sec")
+  })
   
-
 })
+
+fte_theme <- function() {
+  
+  # Generate the colors for the chart procedurally with RColorBrewer
+  palette <- brewer.pal("Greys", n=9)
+  color.background = palette[2]
+  color.grid.major = palette[3]
+  color.axis.text = palette[6]
+  color.axis.title = palette[7]
+  color.title = palette[9]
+  
+  # Begin construction of chart
+  theme_bw(base_size=9) +
+    
+    # Set the entire chart region to a light gray color
+    theme(panel.background=element_rect(fill=color.background, color=color.background)) +
+    theme(plot.background=element_rect(fill=color.background, color=color.background)) +
+    theme(panel.border=element_rect(color=color.background)) +
+    
+    # Format the grid
+    theme(panel.grid.major=element_line(color=color.grid.major,size=.25)) +
+    theme(panel.grid.minor=element_blank()) +
+    theme(axis.ticks=element_blank()) +
+    
+    # Format the legend, but hide by default
+    theme(legend.position="none") +
+    theme(legend.background = element_rect(fill=color.background)) +
+    theme(legend.text = element_text(size=7,color=color.axis.title)) +
+    
+    # Set title and axis labels, and format these and tick marks
+    theme(plot.title=element_text(color=color.title, size=10, vjust=1.25)) +
+    theme(axis.text.x=element_text(size=7,color=color.axis.text)) +
+    theme(axis.text.y=element_text(size=7,color=color.axis.text)) +
+    theme(axis.title.x=element_text(size=8,color=color.axis.title, vjust=0)) +
+    theme(axis.title.y=element_text(size=8,color=color.axis.title, vjust=1.25)) +
+    
+    # Plot margins
+    theme(plot.margin = unit(c(0.35, 0.2, 0.3, 0.35), "cm"))
+}
